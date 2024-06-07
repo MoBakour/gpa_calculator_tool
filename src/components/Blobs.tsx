0fba7e180126f073d0bdb6b228e245ed2233f9f3
Swordax <swordax.sy@gmail.com>
2024-06-07 19:08:12 +0300

    Changes

--- src/components/Blobs.tsx
+++ src/components/Blobs.tsx
@@ -1,37 +1,37 @@
 interface IBlobProps {
-    size: number;
-    color: string;
-    top?: string;
-    bottom?: string;
-    left?: string;
-    right?: string;
+	size: number;
+	color: string;
+	top?: string;
+	bottom?: string;
+	left?: string;
+	right?: string;
 }
 
 export const Blob = ({
-    size,
-    color,
-    top = "auto",
-    bottom = "auto",
-    left = "auto",
-    right = "auto",
+	size,
+	color,
+	top = "auto",
+	bottom = "auto",
+	left = "auto",
+	right = "auto",
 }: IBlobProps) => (
-    <div
-        className="absolute blur-[75px] opacity-15 rounded-full pointer-events-none"
-        style={{
-            width: `${size}px`,
-            height: `${size}px`,
-            backgroundColor: color,
-            top,
-            bottom,
-            left,
-            right,
-        }}
-    ></div>
+	<div
+		className="absolute blur-[75px] opacity-15 rounded-full pointer-events-none"
+		style={{
+			width: `${size}px`,
+			height: `${size}px`,
+			backgroundColor: color,
+			top,
+			bottom,
+			left,
+			right,
+		}}
+	></div>
 );
 
 export const Blobs = () => (
-    <div className="fixed w-screen h-screen overflow-hidden -z-1 pointer-events-none">
-        <Blob size={500} color="purple" right="-200px"></Blob>
-        <Blob size={500} color="cyan" top="200px" left="-300px" />
-    </div>
+	<div className="fixed w-screen h-screen overflow-hidden pointer-events-none">
+		<Blob size={500} color="purple" right="-200px"></Blob>
+		<Blob size={500} color="cyan" top="100px" left="-300px" />
+	</div>
 );
